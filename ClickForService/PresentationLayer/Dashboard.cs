fdfb59c5812daef0a6ff81467e7e4784e6344fda
jnjohnny <19-39366-1@student.aiub.edu>
2021-05-02 15:46:08 +0600

    Fnal changes

--- ClickForService/PresentationLayer/Dashboard.cs
+++ ClickForService/PresentationLayer/Dashboard.cs
@@ -20,7 +20,7 @@ namespace ClickForService.PresentationLayer
             InitializeComponent();
 
 
-            SendNotification.ABC();
+            //SendNotification.ABC();
         }
 
         
